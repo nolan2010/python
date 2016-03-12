6a0f4a128eb77cc51cabecc8885f8555d704fef3
nolan2010 <noland@126.com>
2016-03-12 17:13:52 +0800

    added break sentence where the verification is successed

--- verification.py
+++ verification.py
@@ -7,8 +7,9 @@ while True:
         continue
     age  = int(raw_input("请告诉我你的年龄：").strip())
     job  = raw_input("以及你的工作：").strip()
-    if len(name)==0:
+    if len(job)==0:
         print "please type something"
+        continue
     msg = """
     你的个人信息如下：
     名字：%s
@@ -18,14 +19,15 @@ while True:
     print msg
     if name =='nolan':
         print 'Hi, %s, how are you'%name
+        break
     elif name !='nolan':
         print "Sorry, I dno't know you"
 #   elif raw_input("you wanna type another name?"):
-    name = raw_input("you wanna type another name?")
-    if name =='root':
-        print 'Hi,boss, it is a joke, you know I know you!'
-    elif name =='nolan':
-        print "Come on, %s, why don't you type in at firt time!\n How are you!" %name
-    else:
-        print "Sorry, I really do not know you!"
-    break
+        name = raw_input("you wanna type another name?")
+        if name =='root':
+            print 'Hi,boss, it is a joke, you know I know you!'
+        elif name =='nolan':
+            print "Come on, %s, why don't you type in at firt time!\n How are you!" %name
+        else:
+            print "Sorry, I really do not know you!"
+        break
