a7441c701c6c55942a111f627500f095ef1138bd
nolan2010 <noland@126.com>
2016-03-16 12:04:46 +0800

    added function2 file & 1 example

--- f1.py
+++ f1.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python
 #coding=utf-8
 
-#def sayHi(name):
-#    print "Hello World!"
+##def sayHi(name):
+##    print "Hello World!"
+#
+#def sayHi2(name):
+#    print "Hello, %s, how are you?" %name
+#
+#n = 'Nolan'
+#
+##sayHi(n)
+#sayHi2(n)
+#
 
-def sayHi2(name):
-    print "Hello, %s, how are you?" %name
+#def hello(name):
+#    return 'hello,' + name + '!'
+#print hello('Nolan')
 
-n = 'Nolan'
+##为函数添加文档说明
+#def square(x):
+#  'Calculates the square of the number x.'
+#   return x*x
+#print square.__doc__
+#print  help(square)
 
-#sayHi(n)
-sayHi2(n)
+#def printme(str):
+#    "将一个字符串作为传入参数"
+#    print str
+
+”“”#按值传递参数和按引用传递参数
+#在函数里修改参数,调用时原始的参数即发生改变
+def changeme(mylist):
+    "修改传入的列表"
+    mylist.append([1,2,3,4]);
+    print "函数内取值：", mylist
+    return
+#调用改变后的函数
+mylist = [10,20,30];
+changeme(mylist);
+print"函数外取值：", mylist
+#输出结果一样，因为引用的内容都一样“”“
+
+    
+
+
+
+
+
+
+
