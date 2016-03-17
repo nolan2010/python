6fcdfd7b62fe1da022f05349a6f04a64ee37ded5
nolan2010 <noland@126.com>
2016-03-17 11:17:15 +0800

    instruciton of build in functions

---
+++ bif.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+#数学运算类
+"""
+abs(x)求绝对值
+complex([real[,img]])
+divmod(a,b)取商和余数
+pow(x,y)求幂
+sum(iterable)求一个列表的和
+oct(8)将一个数字转化为8进制
+hex(x)将整数x转化为16进制字符串
+chr(i)返回整数x对应的ASCII字符
+bin(x)将整数x转化为二进制字符串
+bool([x])将x转化为Boolen类型
+"""
+
+#集合类操作
+"""
+unichr(i)返回给定int类型的unicode
+enumerate(sequence[,start=0])返回一个可枚举的对象，该对象的next()方法将返回一个tuple
+iter(o,[,sentinel]) 生成一个对象的迭代器，第二个参数表示分隔符
+max(iterable[,args...][keys]) 返回集合中的最大值
+min(iterable[,args...][keys]) 返回集合中的最小值
+dict[arg]创建数据字典
+list([iterable]) 将一个集合类转化为另外一个集合类
+set()对象实例化
+sum(x)/len(x)求平均值
+sorted(iterable[,args...][keys])排序
+tuple([iterable])生成元组
+"""
+#逻辑判断
+#all
+#any
+#cmp(x,y) x<y 返回负数，相等返回0，大于返回正数
+
+#反射
+callable(object) 检查对象是否可以调用
+dir([object]) 返回对象的所有方法
