3afd8bd8859779e95f1c11545f82e15c5ffa468d
nolan2010 <noland@126.com>
2016-03-18 16:19:21 +0800

    sequence and function

---
+++ day4/mp.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#sentence = raw_input("Sentence: ")
+#
+#screen_width = 80
+#text_width = len(sentence)
+#box_width = text_width + 6
+#left_margin = (screen_width - box_width ) //2
+#
+#print
+#print ' ' * left_margin + '+' + '-' * box_width + '+'
+#print ' ' * left_margin + '|' + ' ' * text_width  + '|'
+#print ' ' * left_margin + '|' +       sentence    + '|'
+#print ' ' * left_margin + '|' + ' ' * text_width  + '|'
+#print ' ' * left_margin + '+' + '-' * box_width  + '+'
+#print
+
+#check function- use for  verification 
+database = [
+    ['nolan', '0929'],
+    ['jonan', '1053'],
+    ['lydia', '1132'],
+    ['ariel', '0217'] 
+] 
+username = raw_input('User name: ')
+pin = raw_input('PIN_code: ')
+
+if [username, pin] in database: print 'Access granted'
+
+
+
+
+
+
+
