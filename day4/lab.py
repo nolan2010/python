1bacb9571fd7f68548b016099b1c94e1330199bc
nolan2010 <noland@126.com>
2016-03-18 11:58:01 +0800

    new version of labs

--- day4/lab.py
+++ day4/lab.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding=utf-8
-a = raw_input("请输入一个字符或数字:")
+a = raw_input("请输入\033[31;1m一个\033[0m字符或数字:")
 b = ord(a)
 c = hex(b)
 print "16进制值为:", c 
--- day4/lab2.py
+++ day4/lab2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #coding=utf-8
-a = raw_input("请输入一个字符：")
-if str.isalpha(a):
+a = raw_input("请输入\033[31;1m一个字母\033[0m：")
+if  str.isalpha(a):
     print "大写格式为：", a.upper() 
 else:
     print "请输入一个字母"
+
